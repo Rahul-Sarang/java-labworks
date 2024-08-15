83d44bbd8a6391aabb54e772088a723d9ee8a210
RahulSarang <ars.rahul.san@gmail.com>
2024-08-15 20:03:37 +0530

    commit and push

---
+++ fibnoacciseries.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class fibnoacciseries {
+    public static void main(String[] args) {
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("series until : ");
+            int no = sc.nextInt();
+            int first = 0;
+            int second = 1;
+            for (int i = 1; i <= no; i++) {
+                System.out.print(first + " ");
+                int nextterm = first + second;
+                first = second;
+                second = nextterm;
+            }
+            System.out.println("");
+        }
+    }
+}
