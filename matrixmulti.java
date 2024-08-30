80c6bb533408f91b16652efa6a557e5221d44403
RahulSarang <ars.rahul.san@gmail.com>
2024-08-30 22:29:41 +0530

    commit and push

--- matrixmulti.java
+++ matrixmulti.java
@@ -5,13 +5,13 @@ public class matrixmulti
     public static void main(String args[])
     {
         Scanner scn= new Scanner(System.in);
-        System.out.println("Enter the row of 1st matrix :");
+        System.out.print("Enter the row of 1st matrix :");
         int row1=scn.nextInt();
-        System.out.println("Enter the column of 1st matrix :");
+        System.out.print("Enter the column of 1st matrix :");
         int col1=scn.nextInt();
-        System.out.println("Enter the row of 2nd matrix :");
+        System.out.print("Enter the row of 2nd matrix :");
         int row2=scn.nextInt();
-        System.out.println("Enter the column of 2nd matrix :");
+        System.out.print("Enter the column of 2nd matrix :");
         int col2=scn.nextInt();
         int matrix1[][]=new int[row1][col1];
         int matrix2[][]=new int[row2][col2];
@@ -25,7 +25,7 @@ public class matrixmulti
         for(int i=0;i<row1;i++)
         {
             for(int j=0;j<col1;j++)
-            {
+            {    System.out.print("Matrix1 ["+i+"]["+j+"] =");
                 matrix1[i][j]=scn.nextInt();
             }
         }
@@ -33,7 +33,7 @@ public class matrixmulti
         for(int i=0;i<row2;i++)
         {
             for(int j=0;j<col2;j++)
-            {
+            {   System.out.print("Matrix2 ["+i+"]["+j+"] =");
                 matrix2[i][j]=scn.nextInt();
             }
         }
